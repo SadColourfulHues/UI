2b6a7547330b7d75d7b50c37c00f8481aec3ff06
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-03-15 01:06:06 +0800

    Implemented base drag and drop controls

--- Interactions/BaseDraggableItem.cs
+++ Interactions/BaseDraggableItem.cs
@@ -5,19 +5,21 @@ namespace SadChromaLib.UI;
 /// <summary>
 /// Base class for draggable control components.
 /// </summary>
-public abstract partial class BaseDraggableItem: Control
+public abstract partial class BaseDraggableItem: Button
 {
+    #region Draggable Item
+
     /// <summary>
     /// <summary>The item's unique 'drag-and-drop function' identifier.</summary>
     /// </summary>
     /// <returns></returns>
-    public abstract string GetDragIdentifier();
+    public abstract StringName GetDragIdentifier();
 
     /// <summary>
     /// Returns the item's drag-and-drop data. Use JSON and SadChromaLib.Persistence for serialising native Godot primitives.
     /// </summary>
     /// <returns></returns>
-    public abstract string GetData();
+    public abstract Godot.Collections.Dictionary<StringName, Variant> GetData();
 
     /// <summary>
     /// Returns whether or not the item can be picked up in the current moment.
@@ -26,4 +28,41 @@ public abstract partial class BaseDraggableItem: Control
     public virtual bool IsDraggable() {
         return true;
     }
+
+    /// <summary>
+    /// Override this to change its 'preview' item.
+    /// </summary>
+    /// <returns></returns>
+    public virtual Control GetDragPreview() {
+        return (Control) Duplicate();
+    }
+
+    #endregion
+
+    #region Configuration
+
+    public override Variant _GetDragData(Vector2 atPosition)
+    {
+        if (!IsDraggable())
+            return default;
+
+        Control preview = GetDragPreview();
+        SetDragPreview(preview);
+
+        return EncodeData();
+    }
+
+    #endregion
+
+    #region Helpers
+
+    private string EncodeData()
+    {
+        return Json.Stringify(new Godot.Collections.Dictionary<StringName, Variant> {
+            ["id"] = GetDragIdentifier(),
+            ["data"] = GetData()
+        });
+    }
+
+    #endregion
 }
--- Interactions/BaseDropTarget.cs
+++ Interactions/BaseDropTarget.cs
@@ -5,12 +5,12 @@ namespace SadChromaLib.UI;
 /// <summary>
 /// Base class for drag-and-drop data consumers.
 /// </summary>
-public abstract partial class BaseDropTarget: Control
+public abstract partial class BaseDropTarget: Panel
 {
+    #region Drop Target
+
     /// <summary>
     /// Returns whether or not a draggable item is compatible with this drop target.
-    /// For fast comparisons, use ReferenceEquals (or SadChromaLib.Utils.Convenience.IDUtils.Test)
-    /// when checking against interned ID strings.
     /// </summary>
     ///
     /// <example>
@@ -23,12 +23,60 @@ public abstract partial class BaseDropTarget: Control
     /// </example>
     /// <param name="id">The identifier of the item being dropped.</param>
     /// <returns></returns>
-    public abstract bool IsItemCompatible(string id);
+    public abstract bool IsItemCompatible(StringName id);
 
     /// <summary>
     /// Processes the dropped data.
     /// </summary>
     /// <param name="id">The identifier of the item being dropped.</param>
     /// <param name="data">The serialised data of the dropped item.</param>
-    public abstract void ConsumeDrop(string id, string data);
+    public abstract void ConsumeDrop(StringName id, Godot.Collections.Dictionary<string, Variant> data);
+
+    #endregion
+
+    #region Configuration
+
+    public override bool _CanDropData(Vector2 atPosition, Variant data)
+    {
+        if (data.VariantType != Variant.Type.String)
+            return false;
+
+        (string id, Godot.Collections.Dictionary<string, Variant> _)?
+        droppedData = DecodeData((string) data);
+
+        if (droppedData is null)
+            return false;
+
+        return IsItemCompatible(droppedData.Value.id);
+    }
+
+    public override void _DropData(Vector2 atPosition, Variant data)
+    {
+        (string id, Godot.Collections.Dictionary<string, Variant> data)?
+        droppedData = DecodeData((string) data);
+
+        if (droppedData is null)
+            return;
+
+        ConsumeDrop(droppedData.Value.id, droppedData.Value.data);
+    }
+
+    #endregion
+
+    #region Helpers
+
+    private (StringName, Godot.Collections.Dictionary<string, Variant>)?
+    DecodeData(string data)
+    {
+        Godot.Collections.Dictionary<string, Variant> passedData = (Godot.Collections.Dictionary<string, Variant>) Json.ParseString(data);
+
+        // Malformed data
+        if (!passedData.ContainsKey("id") || !passedData.ContainsKey("data"))
+            return null;
+
+        return ((StringName) passedData["id"],
+                (Godot.Collections.Dictionary<string, Variant>) passedData["data"]);
+    }
+
+    #endregion
 }
